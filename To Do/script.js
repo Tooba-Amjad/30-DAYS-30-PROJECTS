918330534700189ed4e122ecc6fd3d2266d360ac
Tooba-Amjad <toobaamjad098@gmail.com>
2023-12-03 09:54:40 +0500

    Day 2 To Do App

--- To Do/script.js
+++ To Do/script.js
@@ -1,36 +1,45 @@
-
-
 const taskinput = document.getElementById("input")
 const button = document.getElementById("button");
-
 const tasklist = document.getElementById("tasklist");
+
 function addtask() {
     if (taskinput.value == null || taskinput.value.trim() === '') {
         alert("Input field is empty");
-    }
-    else {
+    } else {
+        // Create a container div for each task
+        const taskContainer = document.createElement('div');
+        taskContainer.classList.add('task-container');
+
         const radio = document.createElement('input');
         radio.type = 'radio';
+
         const li = document.createElement('li');
         const img = document.createElement('img');
         img.src = "remove.png";
         img.alt = 'remove';
 
-        const radio1 = tasklist.appendChild(radio);
-        const value = tasklist.appendChild(li);
-        const img1 = tasklist.appendChild(img);
-        value.textContent = taskinput.value
-        console.log(taskinput.value);
-        taskinput.value = null;
-        radio1.addEventListener("click", () => {
-            li.classList.add('changed');
+        // Append elements to the task container
+        taskContainer.appendChild(radio);
+        taskContainer.appendChild(li);
+        taskContainer.appendChild(img);
 
-        })
+        // Set the task text content
+        li.textContent = taskinput.value;
+
+        // Append the task container to the task list
+        tasklist.appendChild(taskContainer);
+
+        // Clear the input field
+        taskinput.value = null;
+
+        // Add event listeners to radio and img elements
+        radio.addEventListener("click", () => {
+            li.classList.add('changed');
+        });
+
         img.addEventListener("click", () => {
-            tasklist.removeChild(li);
-            tasklist.removeChild(radio1);
-            tasklist.removeChild(img);
-        })
+            tasklist.removeChild(taskContainer);
+        });
     }
 }
 
